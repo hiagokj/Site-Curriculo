a87924d382d9b00122a1916c72a23d3a83d2fd95
Pedro Henrique Silva Freire <psilvafreire@gmail.com>
2025-04-08 22:17:33 -0300

    mudança de estilos

--- curriculoScript.js
+++ curriculoScript.js
@@ -7,7 +7,6 @@ function renderizarCurriculo() {
     window.location.href = "index.html";
     return;
   }
-
   const html = `
       <div class="curriculo-content">
           <img src="${dados.foto}" alt="Foto" class="foto-perfil">
@@ -16,19 +15,32 @@ function renderizarCurriculo() {
               <p><strong>Email:</strong> ${dados.email}</p>
               <p><strong>Telefone:</strong> ${dados.telefone}</p>
               <p><strong>Estado Civil:</strong> ${dados.estadoCivil}</p>
-              <p><strong>Endereço:</strong> ${dados.rua}, ${dados.bairro}, ${
-    dados.cidade
-  }</p>
+              <p><strong>Endereço:</strong> ${dados.rua}, ${dados.bairro}, ${dados.cidade}</p>
               <p><strong>Ensino Superior:</strong> ${dados.superior}</p>
+           
           </div>
           <div class="experiencia-box">
               <h3>Experiência Profissional</h3>
               <p>${dados.experiencia.replace(/\n/g, "<br>")}</p>
           </div>
       </div>
+       <div><button onclick="carregarEstilo('estilo1.css')">Estilo 1</button>
+            <button onclick="carregarEstilo('estilo2.css')">Estilo 2</button>
+            <button onclick="carregarEstilo('style.css')">Estilo 3</button>
+        </div>
   `;
 
   listagem.innerHTML = html;
+ 
 }
-
-window.onload = renderizarCurriculo;
+function carregarEstilo(caminho){
+  document.getElementById("estilo-principal").href = caminho;
+  localStorage.setItem("estilo", caminho);
+}
+window.onload = function() {
+  const estilo = localStorage.getItem("estilo");
+  if(estilo){
+    carregarEstilo(estilo);
+  }
+}
+renderizarCurriculo();
